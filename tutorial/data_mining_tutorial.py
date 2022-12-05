58021fa78bd4dd9f1cce1f42e5823ee4dc767b0e
Brando Miranda <brandojazz@gmail.com>
2022-12-05 12:26:01 -0800

    empty mine data tutorial

---
+++ tutorial/data_mining_tutorial.py
@@ -0,0 +1,22 @@
+"""
+TODO
+
+
+--
+    pycoq_logfile_name: Path = Path(pycoq.config.DEFAULT_CONFIG['log_filename']).expanduser()
+    clear_file_contents(pycoq_logfile_name)
+
+"""
+import time
+
+
+def mine_data():
+    passd
+
+if __name__ == '__main__':
+    start_time = time.time()
+    mine_data()
+    duration = time.time() - start_time
+    print(f"{duration=}\n\a")
+
+    print('---- done!\a\n')
