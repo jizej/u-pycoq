311e16c88ff724a8d6d85de476f0bf98dea88d80
Brando Miranda <brandojazz@gmail.com>
2022-12-05 18:11:25 -0800

    finally installed opam in snap

--- install_coqgym_deps.sh
+++ install_coqgym_deps.sh
@@ -7,12 +7,46 @@ which python
 
 if ! command -v ruby &> /dev/null
 then
-    # First, install Ruby, as that is for some reason required to build
-    # the "system" project
-    git clone https://github.com/rbenv/ruby-build.git ~/ruby-build
+    echo "Going to try to install ruby (ideally 3.1.2)"
+    ruby -v
+#    # First, install Ruby, as that is for some reason required to build
+#    # the "system" project
+#    git clone https://github.com/rbenv/ruby-build.git ~/ruby-build
+#    mkdir -p ~/.local
+#    PREFIX=~/.local ./ruby-build/install.sh
+#    ~/.local/ruby-build 3.1.2 ~/.local/
+    mkdir ~/.rbenv
+    cd ~/.rbenv
+    git clone https://github.com/rbenv/rbenv.git .
+
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    eval "$(rbenv init -)"
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc.user
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc.user
+    #    exec $SHELL
+    # bash
+
+    rbenv -v
+
+    # - install ruby-build
+    mkdir ~/.ruby-build
+    cd ~/.ruby-build
+    git clone https://github.com/rbenv/ruby-build.git .
+
+    export PATH="$HOME/.ruby-build/bin:$PATH"
+    echo 'export PATH="$HOME/.ruby-build/bin:$PATH"' >> ~/.bashrc.user
+    #    exec $SHELL
+    # bash
+
+    ruby-build --version
+
+    # - install ruby without sudo -- now that ruby build was install
     mkdir -p ~/.local
-    PREFIX=~/.local ./ruby-build/install.sh
-    ~/.local/ruby-build 3.1.2 ~/.local/
+    #    ruby-build 3.1.2 ~/.local/
+    rbenv install 3.1.2
+    rbenv global 3.1.2
+
+    ruby -v
 fi
 
 git submodule update && git submodule init
