5f23f2bd64d2bef2c76c29d6e4b5693c7dfb4c7c
Brando Miranda <brandojazz@gmail.com>
2023-03-22 07:04:27 -0700

    install rlwrap locally for linux, better since we arent doing pycoq in mac so this tests in real env. ess eff

---
+++ tutorial/tutorial_ibm_pycoq_loop_through_data.py
@@ -0,0 +1,33 @@
+from typing import Union
+
+import pycoq
+from pycoq.common import CoqContext
+from pycoq.opam import strace_build_coq_project_and_get_filenames
+from pycoq.project_splits import get_proj_splits_based_on_name_of_path2data, CoqProjs
+from pycoq.serapi import execute
+from pycoq.utils import get_coq_serapi
+
+
+def example_execute_coq_files_from_coq_proj_in_pycoq(path2data: str = '~/data/lf_proj/'):
+    coq_proj: CoqProjs = get_proj_splits_based_on_name_of_path2data(path2data)
+    path2filenames_raw: list[str] = strace_build_coq_project_and_get_filenames(coq_proj, make_clean_coq_proj=True)
+    path2filename: str
+    for path2filename in path2filenames_raw:
+        coq_ctxt: CoqContext = pycoq.common.load_context(path2filename)
+        async with get_coq_serapi(coq_ctxt) as coq:
+            stmts_in_file: iter[str] = pycoq.split.coq_stmts_of_context(coq_ctxt)
+            for stmt_id, stmt in enumerate(stmts_in_file):
+                goals: Union[str, list] = await execute(stmt, coq)
+
+
+if __name__ == '__main__':
+    import time
+
+    start_time = time.time()
+    # - compile coq proj files in pycoq
+    example_execute_coq_files_from_coq_proj_in_pycoq('~/data/lf_proj/')
+    example_execute_coq_files_from_coq_proj_in_pycoq('~/data/coqgym/')
+
+    # - done
+    duration = time.time() - start_time
+    print(f"Done! {duration=}\n\a")
